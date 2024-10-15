ef8170d95e43ee8d8fa419c4261a77feaf6f0b48
李伟(Michael) <michaelAip@gmail.com>
2024-10-15 20:52:12 +0800

    feat: 初始化App类，作为主入口并集成Solon框架。

---
+++ alinesno-tools-crawler/src/main/java/com/alinesno/infra/data/scheduler/tool/App.java
@@ -0,0 +1,17 @@
+package com.alinesno.infra.data.scheduler.tool;
+
+import lombok.extern.slf4j.Slf4j;
+import org.noear.solon.Solon;
+import org.noear.solon.annotation.Init;
+import org.noear.solon.annotation.Inject;
+import org.noear.solon.annotation.SolonMain;
+
+@Slf4j
+@SolonMain
+public class App {
+
+    public static void main(String[] args) {
+        Solon.start(App.class, args);
+    }
+
+}
