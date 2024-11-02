b1bd3d54c60adcf1a320ee2ee01b9484835b22ed
陈伟(Victor) <victorAip@gmail.com>
2024-11-03 00:14:43 +0800

    改进: 移除 WebCrawler 类中不必要的并发执行代码。

--- alinesno-tools-crawler/src/main/java/com/alinesno/infra/data/scheduler/tool/crawler/WebCrawler.java
+++ alinesno-tools-crawler/src/main/java/com/alinesno/infra/data/scheduler/tool/crawler/WebCrawler.java
@@ -18,9 +18,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 /**
  * WebCrawler类用于实现网页爬虫的功能
