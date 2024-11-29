02e8c4587097266d4042f5964d91c8f2a7b6a0f9
李伟(Michael) <michaelAip@gmail.com>
2024-11-30 00:17:04 +0800

    feat:新增 `CodeClone` 类，提供对git代码库进行克隆分析的方法。

---
+++ alinesno-tools-code-scan/src/main/java/com/alinesno/infra/data/scheduler/tools/scan/CodeClone.java
@@ -0,0 +1,16 @@
+package com.alinesno.infra.data.scheduler.tools.scan;
+
+import lombok.SneakyThrows;
+
+/**
+ * 提供出方法，对git clone下来的代码进行克隆分析，传递git地址和分支
+ */
+public class CodeClone {
+
+    @SneakyThrows
+    public static void syncGitee(String sourceGitRepo, String repoName){
+
+
+    }
+
+}
