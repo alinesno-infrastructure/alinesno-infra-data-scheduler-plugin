85f39e0877c395f05d8c764e6621499e48802ebf
李伟(Michael) <michaelAip@gmail.com>
2024-11-30 00:17:04 +0800

    feat: 添加 App 主类以启动项目。

---
+++ alinesno-tools-code-scan/src/main/java/com/alinesno/infra/data/scheduler/tools/App.java
@@ -0,0 +1,11 @@
+package com.alinesno.infra.data.scheduler.tools;
+
+import org.noear.solon.Solon;
+import org.noear.solon.annotation.SolonMain;
+
+@SolonMain
+public class App {
+    public static void main(String[] args) {
+        Solon.start(App.class, args);
+    }
+}
